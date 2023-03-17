5f280bd86c5710e24c9b6afacfd8c67677ab37f5
JordanWitt <wittjordan94@gmail.com>
2023-03-17 13:03:27 -0500

    created home default page

---
+++ src/main/java/com/xplat/crossplatform/CrossPlatformApplication.java
@@ -0,0 +1,13 @@
+package com.xplat.crossplatform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrossPlatformApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CrossPlatformApplication.class, args);
+    }
+
+}
